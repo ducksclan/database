07682b36f751d78e977055592eae4e05c8e9270e
hitachi567 <abidai790@gmail.com>
2022-05-13 21:15:44 +0300

    export were added

---
+++ src/library/index.ts
@@ -0,0 +1,6 @@
+import SerialEntity from './serial.entity';
+import BaseEntity from './base.entity';
+import UuidEntity from './uuid.entity';
+import Database from './database';
+
+export { BaseEntity, SerialEntity, UuidEntity, Database };
